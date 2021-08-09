84eb3950c15a8c079116f357f88d4fa88eff2a9d
LucaBia <gian.luca.99@hotmail.com>
2021-08-08 23:03:14 -0600

    Sign Up

--- client.py
+++ client.py
@@ -1,3 +1,85 @@
+import slixmpp
+from slixmpp import ClientXMPP
+from slixmpp.exceptions import IqError, IqTimeout
+
+import logging
+from getpass import getpass
+from argparse import ArgumentParser
+
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
+
+class Client(ClientXMPP):
+    def __init__(self, username, password):
+        ClientXMPP.__init__(self, username, password)
+        self.username_ = username
+        self.password = password
+
+        self.add_event_handler("register", self.signup)
+
+
+    async def signup(self, iq):
+        self.send_presence()
+        self.get_roster()
+
+        """
+        Fill out and submit a registration form.
+        The form may be composed of basic registration fields, a data form,
+        an out-of-band link, or any combination thereof. Data forms and OOB
+        links can be checked for as so:
+        if iq.match('iq/register/form'):
+            # do stuff with data form
+            # iq['register']['form']['fields']
+        if iq.match('iq/register/oob'):
+            # do stuff with OOB URL
+            # iq['register']['oob']['url']
+        To get the list of basic registration fields, you can use:
+            iq['register']['fields']
+        """
+        
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            await resp.send()
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
+
+
+# def signup(username, password):
+    # client = Client(username, password)
+
+    # client.register_plugin("xep_0030")
+    # client.register_plugin("xep_0004")
+    # client.register_plugin("xep_0199")
+    # client.register_plugin("xep_0066")
+    # client.register_plugin("xep_0077")
+
+    # client["xep_0077"].force_registration = True
+
+    # client.connect()
+    # client.process()
+
+
+
+# class SendMsgBot(slixmpp.ClientXMPP):
+
+#     def __init__(self, jid, password, recipient, message):
+#         slixmpp.ClientXMPP.__init__(self, jid, password)
+
+
+    
+#     def SignUp(usr, psw):
+#         return None
+
 
 
 loop = True
@@ -12,7 +94,25 @@ while loop:
         """)
     option = int(input("Choose an option to continue: "))
     if option == 1:
-        print("QUE ONDA")
+        print("Opcion 1")
+        user = input("Username (user@alumchat.xyz): ")
+        password = input("Password: ")
+        # signup(user, password)
+
+        client = Client(user, password)
+
+        client.register_plugin("xep_0030")
+        client.register_plugin("xep_0004")
+        client.register_plugin("xep_0199")
+        client.register_plugin("xep_0066")
+        client.register_plugin("xep_0077")
+
+        client["xep_0077"].force_registration = True
+
+        client.connect()
+        client.process()
+
+        print("******************************HOLAAAAAA***************************")
     elif option == 2:
         loginloop = True
         while loginloop:
